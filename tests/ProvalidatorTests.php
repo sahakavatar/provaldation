1ceb0f8de4e4fe46896a5733ef2d0e5f3b59b565
Menq <sahak@ukdevplus.co.uk>
2017-07-07 01:08:21 +0400

    up

---
+++ tests/ProvalidatorTests.php
@@ -0,0 +1,16 @@
+<?php Avatar\ProValid\tests;
+
+/**
+ * Created by PhpStorm.
+ * User: menq
+ * Date: 7/7/17
+ * Time: 1:04 AM
+ */
+class ProvalidatorTests
+{
+    public function test()
+    {
+        return "OK";
+
+}
+}
