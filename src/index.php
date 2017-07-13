51f391fefeed555462709b74cb257b03d8f6278c
Menq <sahak@ukdevplus.co.uk>
2017-07-13 19:41:40 +0400

    test

--- src/index.php
+++ src/index.php
@@ -3,3 +3,11 @@
 
 
 
+\Eventy::action('admin.menus', [
+    "title" => "ProValidator",
+    "custom-link" => "#",
+    "icon" => "fa fa-folder-open",
+    "is_core" => "yes",
+    "main" => true,
+    "children" => []
+]);
