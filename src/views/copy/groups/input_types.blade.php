fd1e60ffe9061cdbef6a1e87ddab5d303d94025d
sahakavatar <hakobyan.sahak88@gmail.com>
2018-03-01 14:42:26 +0400

    UPDATE

--- src/views/copy/groups/input_types.blade.php
+++ src/views/copy/groups/input_types.blade.php
@@ -4,10 +4,21 @@
         <option value="0">Select Rule</option>
         <option value="alpha">Alpha</option>
         <option value="integer">Integer</option>
+        <option value="alpha_dash">Alpha Dash</option>
+        <option value="alpha_num">Alpha Num</option>
+        <option value="email">Email</option>
+        <option value="string">String</option>
+        <option value="active_url">Active URL</option>
+        <option value="url">URL</option>
+        <option value="exists">Exists</option>
         <option value="size">Size</option>
         <option value="characters">Characters</option>
         <option value="digits">Digits</option>
         <option value="boolean">Boolean</option>
+        <option value="date">Date</option>
+        <option value="timezone">Timezone</option>
+        <option value="file">File</option>
+        <option value="ip">IP</option>
     </select>
 </div>
 
